3ee5cd8f7e1fcfb457dd7269351a01c58845d567
zia.fazal@arbisoft.com <zia.fazal@arbisoft.com>
2022-02-10 14:35:17 +0500

    Ability to configure edx-ace with course emails
    fix: fix typo

---
+++ lms/djangoapps/bulk_email/message_types.py
@@ -0,0 +1,13 @@
+"""
+ACE message types for bulk course emails.
+"""
+
+from openedx.core.djangoapps.ace_common.message import BaseMessageType
+
+
+class BulkEmail(BaseMessageType):
+    """
+    Course message to list of recepient by instructors.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
